b6aca8e32318c5e14cc9d7c0067f75c03b986718
OngNyang <jhj951105@gmail.com>
2025-02-25 22:51:06 +0900

    Feat: create endpoint(POST, /crawl)

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+from crawler import naver_map_crawler
 
 app = Flask(__name__)
 
@@ -7,6 +9,17 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/crawl', methods=['POST'])
+def crawl():
+    data = request.json
+    search_query = data.get('query')
+
+    if not search_query:
+        return jsonify({'error': 'No search query provided'}), 400
+
+    result = naver_map_crawler(search_query)
+
+    return jsonify({"message": result, "file": "output/results.xlsx"})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
